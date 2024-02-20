3acb69df50e28dcb7d7fed45c661f7637ece7e6d
clock <1162411462@qq.com>
2024-02-20 18:07:35 +0800

    继续完善功能,优化部分qt显示

---
+++ cmake/grpc_manager/client/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/linux_distribute_monitor/grpc_manager/client/src/grpc_client.cpp" "grpc_manager/client/CMakeFiles/client.dir/src/grpc_client.cpp.o" "gcc" "grpc_manager/client/CMakeFiles/client.dir/src/grpc_client.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake/grpc_manager/server/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/src/grpc_manager.cpp.o"
+  "CMakeFiles/server.dir/src/grpc_manager.cpp.o.d"
+  "CMakeFiles/server.dir/src/main.cpp.o"
+  "CMakeFiles/server.dir/src/main.cpp.o.d"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
